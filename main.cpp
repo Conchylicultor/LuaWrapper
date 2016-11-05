5c1cd80eed162cfc233303a73e0ed45032ec5846
Conchylicultor <etiennefg.pot@gmail.com>
2016-11-04 18:47:42 -0700

    Refactor memleak test

--- main.cpp
+++ main.cpp
@@ -15,24 +15,17 @@ void unit_test(lua_State* L)
 }
 
 
-/** Sample code for the wrapper
+/** Loop over forward with different inputs tensors,
+  * The memory should stay constant
   */
-int main(int argc, char** argv)
+void test_memleak()
 {
-    (void)argc;
-    (void)argv;
-
-    cout << "Lua/torch wrapper test" << endl;
-
     lua_State* L = LuaWrap::init_torch_vm();
     int model_nin = LuaWrap::load_model(L, "/home/teradeep/etienne/LuaWrapper/model_185_bnabs.net");
     LuaWrap::call_lua_method(L, model_nin, "evaluate"); // model:evaluate()
 
     for(int i = 0 ; i < 100000 ; ++i)
     {
-        //THByteTensor* inputB = LuaWrap::THByte_create_tensor(1096, 1096);
-        //LuaWrap::THByte_push_tensor(L, inputB);
-
         THFloatTensor* input = LuaWrap::THFloat_create_tensor(96, 96);
         LuaWrap::THFloat_push_tensor(L, input);
 
@@ -40,6 +33,8 @@ int main(int argc, char** argv)
 
         LuaWrap::call_lua_method(L, model_nin, "forward", 1, 1); // model:forward(input)
 
+        // If the garbadge collector isn't called form time to time, the memory will keep growing
+        // Calling it every iterations will strongly affect the performances
         if(i % 1000 == 0)
         {
             lua_gc(L, LUA_GCCOLLECT, 0);
@@ -50,6 +45,30 @@ int main(int argc, char** argv)
 
         std::cout << i << " " << lua_gc (L, LUA_GCCOUNT, 0) << " " << lua_gc (L, LUA_GCCOUNTB, 0) << std::endl;
     }
+
+    lua_close(L);
+}
+
+
+/** Calling forward through a lua script
+  */
+void test_memleak_script()
+{
+
+}
+
+
+/** Sample code for the wrapper
+  */
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv; // Unused
+
+    cout << "Lua/torch wrapper test" << endl;
+
+    test_memleak();
+    test_memleak_script();
 
     cout << "The End" << endl;
 
